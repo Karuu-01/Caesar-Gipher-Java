f5c61940b6864bd5b35543f9fc6b7e15f4efb22e
karuu-01 <brian.muchori@student.moringaschool.com>
2021-08-01 02:14:57 +0300

    add changes to readme file

--- src/main/java/decryption.java
+++ src/main/java/decryption.java
@@ -1,2 +1,20 @@
 public class decryption {
+    private static String decrypt(String cipherText, int shiftKey)
+    {
+        final String ALPHABET = "abcdefghijklmnopqrstuvwxyz";
+        cipherText = cipherText.toLowerCase();
+        String plainText = "";
+        for (int i = 0; i < cipherText.length(); i++)
+        {
+            int charPosition = ALPHABET.indexOf(cipherText.charAt(i));
+            int keyVal = (charPosition - shiftKey) % 26;
+            if (keyVal < 0)
+            {
+                keyVal = ALPHABET.length() + keyVal;
+            }
+            char replaceVal = ALPHABET.charAt(keyVal);
+            plainText += replaceVal;
+        }
+        return plainText;
+    }
 }
--- src/main/java/encryption.java
+++ src/main/java/encryption.java
@@ -1,2 +1,17 @@
 public class encryption {
+    public static String encrypt(String plainText, int shiftKey)
+    {
+        final String ALPHABET = "abcdefghijklmnopqrstuvwxyz";
+
+        plainText = plainText.toLowerCase();
+        String cipherText = " ";
+        for (int i = 0; i < plainText.length(); i++)
+        {
+            int charPosition = ALPHABET.indexOf(plainText.charAt(i));
+            int keyVal = (shiftKey + charPosition) % 26;
+            char replaceVal = ALPHABET.charAt(keyVal);
+            cipherText += replaceVal;
+        }
+        return cipherText;
+    }
 }
