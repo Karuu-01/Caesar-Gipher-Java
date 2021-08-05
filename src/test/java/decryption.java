0b23bad40c9ed5f98e8e662bf04b20bed483524c
karuu-01 <brian.muchori@student.moringaschool.com>
2021-08-05 15:18:59 +0300

    add changes

--- src/test/java/decryption.java
+++ src/test/java/decryption.java
@@ -1,2 +1,37 @@
-package PACKAGE_NAME;public class decryption {
+import org.junit.Test;
+
+public class decryption {
+    final String ALPHABET = "abcdefghijklmnopqrstuvwxyz";
+
+    @Test
+    public void checkForAlphabet() {
+        final String ALPHABET = "abcdefghijklmnopqrstuvwxyz";
+
+    }
+
+    @Test
+    public void checkForDecryption() {}
+        public String checkStringConvertsToCharacter () {
+            String cipherText = null;
+
+
+            cipherText = cipherText.toLowerCase();
+            StringBuilder plainText = new StringBuilder();
+            for (int i = 0; i < cipherText.length(); i++) {
+                int charPosition = ALPHABET.indexOf(cipherText.charAt(i));
+                int keyVal = (charPosition - 25) % 26;
+                if (keyVal < 0) {
+                    keyVal = ALPHABET.length() + keyVal;
+                }
+                char replaceVal = ALPHABET.charAt(keyVal);
+                plainText.append(replaceVal);
+            }
+            return plainText.toString();
+        }
+
+    @Test
+    public void checkForEncrypt() {
+        String cipherText = null;
+
+    }
 }
